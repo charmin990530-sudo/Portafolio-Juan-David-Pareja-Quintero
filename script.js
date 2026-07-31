e74c5cf725c6461bfbb68f05f800ffcf0ecfbf8a
Juanda <charmin990530@gmail.com>
2026-07-31 16:29:47 -0500

    actualizacion intentando java

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ if (formContacto) {
         window.location.href = 'gracias.html';
     })
 }
+
+const carrusel = document.getElementById('carrusel');
+const btnAnterior = document.getElementById('btn-anterior');
+const btnSiguiente = document.getElementById('btn-siguiente');
+
+if (carrusel && btnAnterior && btnSiguiente) {
+    const distanciaScroll = 300;
+
+    btnSiguiente.addEventListener('click', () => {
+        carrusel.scrollBy({ left: distanciaScroll, behavior: 'smooth' });
+    });
+
+    btnAnterior.addEventListener('click', () => {
+        carrusel.scrollBy({ left: -distanciaScroll, behavior: 'smooth' });
+    });
+}
