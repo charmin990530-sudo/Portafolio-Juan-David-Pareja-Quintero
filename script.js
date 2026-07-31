14c785b6eed5f003938820cf021ce209f2a14f4a
Juanda <charmin990530@gmail.com>
2026-07-31 16:43:26 -0500

    intentando poner modo noche con las variables

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ if (carrusel && btnAnterior && btnSiguiente) {
         carrusel.scrollBy({ left: -distanciaScroll, behavior: 'smooth' });
     });
 }
+
+const tarjetasProyecto = document.querySelectorAll('.proyecto');
+
+tarjetasProyecto.forEach(tarjeta => {
+    tarjeta.addEventListener('mousemove', (evento) => {
+        const rect = tarjeta.getBoundingClientRect();
+        const x = evento.clientX - rect.left;
+        const y = evento.clientY - rect.top;
+
+        const centroX = rect.width / 2;
+        const centroY = rect.height / 2;
+
+        const rotarX = ((y - centroY) / centroY) * -8;
+        const rotarY = ((x - centroX) / centroX) * 8;
+
+        tarjeta.style.transform =
+            `perspective(1000px) rotateX(${rotarX}deg) rotateY(${rotarY}deg) scale(1.03)`;
+    });
+
+    tarjeta.addEventListener('mouseleave', () => {
+        tarjeta.style.transform =
+            'perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1)';
+    });
+});
